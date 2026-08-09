67f4937ffc3a0ad042d8f2267295dae00fd648e2
Keith <kru1026@gmail.com>
2026-08-09 11:19:29 -0400

    Testing15

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,21 @@
 const express = require("express");
 const messageRoutes = require("./routes/messages");
+const cors = require("cors");
 
 const app = express();
 
 app.use(express.json());
+
+app.use(cors({
+  origin: [
+    "http://localhost:3000",
+    "https://intro2nd.vercel.app"
+  ]
+}));
+
+app.get("/healthCheck", (req, res) => {
+    res.status(200).json({ name: 'John Doe' })
+});
 
 app.use("/api/messages", messageRoutes);
 
--- backend/server.js
+++ backend/server.js
@@ -2,19 +2,7 @@ require("dotenv").config();
 
 const app = require("./app");
 const connectDB = require("./config/mongoDB");
-const cors = require("cors");
 const PORT = process.env.PORT || 8080;
-
-app.use(cors({
-  origin: [
-    "http://localhost:3000",
-    "https://intro2nd.vercel.app"
-  ]
-}));
-
-app.get("/healthCheck", (req, res) => {
-    res.status(200).json({ name: 'John Doe' })
-});
 
 async function startServer() {
   await connectDB();
