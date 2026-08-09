e83efd4b3a82f056c57931b59d18108299080076
Keith <kru1026@gmail.com>
2026-08-08 22:42:53 -0400

    Testing11

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,12 @@ const messageRoutes = require("./routes/messages");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:3000",
+    "https://intro2nd.vercel.app"
+  ]
+}));
 
 app.use(express.json());
 
