2c7538e1eb150be6699c85d116a46104d0f9683b
Keith <kru1026@gmail.com>
2026-08-08 21:31:25 -0400

    Testing3

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
+app.get("/healthCheck", (req, res) => {
     res.status(200).json({ name: 'John Doe' })
 });
 
--- my-next-app/pages/index.js
+++ my-next-app/pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
 
   async function healthCheck() {
     try {
-        const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/`, {
+        const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/healthCheck`, {
             method: 'GET',
         });
         if (!res.ok) {
